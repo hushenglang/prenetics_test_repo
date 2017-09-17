8e56195b9dd1800c9f994e91e6e4fa81e4eea0fa
hushenglang <hushenglang@gmail.com>
2017-09-17 16:17:25 +0800

    add more test case

--- test/testAccount.js
+++ test/testAccount.js
@@ -29,7 +29,21 @@ describe("Account Testing", function() {
 
         });
 
-        it("Retrive genetic result, return result object", function(done){
+        it("Retrive account profile fail without token provided", function(done){
+            const profileApi = "/api/account/profile";
+            request
+                .get(profileApi)
+                .expect(403)
+                .expect(function(response){
+                    expect(response.body.success).eql(false);
+                })
+                .end(function(err, res) {
+                    done(err);
+                });
+
+        });
+
+        it("Retrive genetic results, return result object", function(done){
             const geneticResultApi = "/api/account/genetic_results";
             const token = "Bearer "+request.authToken;
             request
@@ -43,7 +57,19 @@ describe("Account Testing", function() {
                 .end(function(err, res) {
                     done(err);
                 });
+        });
 
+        it("Retrive genetic results fail without token provided", function(done){
+            const geneticResultApi = "/api/account/genetic_results";
+            request
+                .get(geneticResultApi)
+                .expect(403)
+                .expect(function(response){
+                    expect(response.body.success).eql(false);
+                })
+                .end(function(err, res) {
+                    done(err);
+                });
         });
     })
 });
